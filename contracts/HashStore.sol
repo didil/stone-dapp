d12ccb5b102a89787d25d98728f2846a8895a647
didil <haritahadil@yahoo.fr>
2017-12-21 12:12:15 +0100

    contract refactor + docs

--- contracts/HashStore.sol
+++ contracts/HashStore.sol
@@ -1,60 +1,115 @@
 pragma solidity ^0.4.11;
 
-
 contract HashStore {
-    struct Hash {
+  /*
+  *  Events
+  */
+  event OwnershipTransferred(address indexed _previousOwner, address indexed _newOwner);
+  event NewHashStored(address indexed _hashSender, uint _hashId, string _hashContent, uint timestamp);
+  event Withdrawn(address indexed _hashSender, uint amount);
+
+  /*
+  * Storage
+  */
+
+  struct Hash {
+    // sender address
     address sender;
+    // hash text
     string content;
+    // creation timestamp
     uint timestamp;
-    }
+  }
 
-    mapping (uint => Hash) public hashes;
-    address public owner;
-    uint public lastHashId;
-    uint public price;
+  // Hashes mapping
+  mapping(uint => Hash) public hashes;
+  // Contract owner
+  address public owner;
+  // Last stored Hash Id
+  uint public lastHashId;
+  // Service price in Wei
+  uint public price;
 
-    event OwnershipTransferred(address indexed _previousOwner, address indexed _newOwner);
-    event NewHashStored(address indexed _hashSender, uint _hashId, string _hashContent, uint timestamp);
-    event Withdrawn(address indexed _hashSender, uint amount);
+  /*
+  * Modifiers
+  */
 
-    function HashStore(uint _price) public {
-        require(_price > 0);
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-        owner = msg.sender;
-        price = _price;
-        lastHashId = 0;
-    }
+  /*
+  * Public functions
+  */
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+  /**
+  * @dev Contract constructor
+  * @param _price Service price
+  */
+  function HashStore(uint _price) public {
+    // check price valid
+    require(_price > 0);
 
-    function transferOwnership(address newOwner) onlyOwner public {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
+    // assign owner
+    owner = msg.sender;
+    // assign price
+    price = _price;
+    // init ids
+    lastHashId = 0;
+  }
 
-    function withdrawBalance() onlyOwner public {
-        var amount = this.balance;
-        owner.transfer(this.balance);
-        Withdrawn(owner, amount);
-    }
+  /**
+  * @dev Transfer contract ownership
+  * @param _newOwner New owner address
+  */
+  function transferOwnership(address _newOwner) onlyOwner public {
+    // check address not null
+    require(_newOwner != address(0));
 
-    function save(string hashContent) payable public {
-        // only save if service price paid
-        require(msg.value >= price);
+    // assign new owner
+    owner = _newOwner;
 
-        uint hashId = ++lastHashId;
-        hashes[hashId].sender = msg.sender;
-        hashes[hashId].content = hashContent;
-        hashes[hashId].timestamp = block.timestamp;
+    // Log event
+    OwnershipTransferred(owner, _newOwner);
+  }
 
-        NewHashStored(hashes[hashId].sender, hashId, hashes[hashId].content, hashes[hashId].timestamp);
-    }
+  /**
+  * @dev Withdraw contract accumulated Eth balance
+  */
+  function withdrawBalance() onlyOwner public {
+    var amount = this.balance;
 
-    function find(uint hashId) constant public returns (address hashSender, string hashContent, uint hashTimestamp) {
-        return (hashes[hashId].sender, hashes[hashId].content, hashes[hashId].timestamp);
-    }
+    // transfer balance
+    owner.transfer(this.balance);
+
+    // Log event
+    Withdrawn(owner, amount);
+  }
+
+  /**
+  * @dev save new hash
+  * @param _hashContent Hash Content
+  */
+  function save(string _hashContent) payable public {
+    // only save if service price paid
+    require(msg.value >= price);
+
+    // create Hash
+    uint hashId = ++lastHashId;
+    hashes[hashId].sender = msg.sender;
+    hashes[hashId].content = _hashContent;
+    hashes[hashId].timestamp = block.timestamp;
+
+    // Log event
+    NewHashStored(hashes[hashId].sender, hashId, hashes[hashId].content, hashes[hashId].timestamp);
+  }
+
+  /**
+  * @dev find hash by id
+  * @param _hashId Hash Id
+  */
+  function find(uint _hashId) constant public returns (address hashSender, string hashContent, uint hashTimestamp) {
+    return (hashes[_hashId].sender, hashes[_hashId].content, hashes[_hashId].timestamp);
+  }
 }
